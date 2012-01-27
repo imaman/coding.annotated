01fb8acb955ee73289efb51d6e72b4ea84ddfd27
Itay Maman <itay.maman@gmail.com>
2012-01-27 15:43:41 +0200

    the liveness rules

---
+++ life.rb
@@ -0,0 +1,32 @@
+describe "liveness rules" do
+  it "a live cell stays live if it has two neighbors" do
+    Life.next_state(true, 2).should == true
+  end
+  it "a live cell dies if it has less than two neighbors" do
+    Life.next_state(true, 1).should == false
+  end
+  it "a live cell stays live if it has three neighbors" do
+    Life.next_state(true, 3).should == true
+  end
+  it "a live cell dies if it has more than three neighbors" do
+    Life.next_state(true, 4).should == false
+  end
+  it "a dead cell stays dead if it has two neighbors" do
+    Life.next_state(false, 2).should == false
+  end
+  it "a dead cell comes back life if it has three neighbors" do
+    Life.next_state(false, 3).should == true
+  end
+end
+
+
+class Life
+  def self.next_state(is_live, n)
+    if is_live
+      n == 2 || n == 3
+    else
+      n == 3
+    end
+  end
+end
+
