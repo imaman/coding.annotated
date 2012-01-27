c1f58b2383183725c9ecfb8d954948efd1bb78a0
Itay Maman <itay.maman@gmail.com>
2012-01-27 18:25:30 +0200

    at this point Cell.living_neighbors is a hack - just returns grid.length. this is due to the order I wrote my tests. Had I written them differently (e.g., the 2nd rspec for this method would place some cells outside the neighborhood so a grid.length would not suffice) I already had a full blown  impl. The trick in TDD is to think about the tests that will force you to move forward but as little as possible. anybody can come up with a monster test that will force you to finish the app, but only few can write a test that will move you forward just a bit. Another issue is that you don't want a test that is already answered by the current impl.

--- life.rb
+++ life.rb
@@ -1,30 +1,105 @@
 describe "liveness rules" do
   it "a live cell stays live if it has two neighbors" do
-    Life.next_state(true, 2).should == true
+    Life.next_state(true, 2).should == Life.live
   end
   it "a live cell dies if it has less than two neighbors" do
-    Life.next_state(true, 1).should == false
+    Life.next_state(true, 1).should == Life.dead
   end
   it "a live cell stays live if it has three neighbors" do
-    Life.next_state(true, 3).should == true
+    Life.next_state(true, 3).should == Life.live
   end
   it "a live cell dies if it has more than three neighbors" do
-    Life.next_state(true, 4).should == false
+    Life.next_state(true, 4).should == Life.dead
   end
   it "a dead cell stays dead if it has two neighbors" do
-    Life.next_state(false, 2).should == false
+    Life.next_state(false, 2).should == Life.dead
   end
   it "a dead cell comes back life if it has three neighbors" do
-    Life.next_state(false, 3).should == true
+    Life.next_state(false, 3).should == Life.live
   end
 end
 
+describe "cell" do
+  it "knows its neighbors" do
+    Cell.new(10, 20).neighbors.should include Cell.new(9,19)
+    Cell.new(10, 20).neighbors.should include Cell.new(9,20)
+    Cell.new(10, 20).neighbors.should include Cell.new(9,21)
+    Cell.new(10, 20).neighbors.should include Cell.new(10,19)
+    Cell.new(10, 20).neighbors.should include Cell.new(10,21)
+    Cell.new(10, 20).neighbors.should include Cell.new(11,19)
+    Cell.new(10, 20).neighbors.should include Cell.new(11,20)
+    Cell.new(10, 20).neighbors.should include Cell.new(11,21)
+  end
+
+  describe "counts living neighbors" do
+    it "returns 0 when grid is empty" do
+      Cell.new(100,100).live_neighbors([]).should == 0
+    end
+    it "returns living cells which are neighbors" do
+      Cell.new(100,100).live_neighbors([ Cell.new(99,99), Cell.new(101,101)]).should == 2
+    end
+  end
+
+end
+
+class Cell
+  def initialize(x,y)
+    @x = x
+    @y = y
+  end
+
+  def live_neighbors(grid)
+    grid.length
+  end
+
+  def neighbors 
+    [ Cell.new(@x-1, @y-1), Cell.new(@x-1, @y), Cell.new(@x-1, @y+1), 
+      Cell.new(@x, @y-1), Cell.new(@x, @y+1), 
+      Cell.new(@x+1, @y-1), Cell.new(@x+1, @y), Cell.new(@x+1, @y+1) ]
+  end
+
+  def x 
+    @x
+  end
+
+  def y
+    @y
+  end
+
+  def ==(that)
+    @x == that.x && @y == that.y
+  end
+end
+
+class Dead
+  def next(n)
+    if n == 3 then Life.live else Life.dead end
+  end
+end
+
+class Live
+  def next(n) 
+    if n == 2 || n == 3 then Life.live else Life.dead end
+  end
+end
 
 class Life
+  @@dead = Dead.new()
+  @@live = Live.new()
+
+  def self.live
+    @@live
+  end
+
+  def self.dead
+    @@dead
+  end
+
   def self.next_state(is_live, n)
-    # Now let's reorganize the rules by n
-    return true if n == 3
-    return n == 2 && is_live
+    # Reorganize again, this time via objects representing the state (dead or
+    # alive).
+    state = if is_live then live else dead end
+    return state.next(n)
   end
 end
 
