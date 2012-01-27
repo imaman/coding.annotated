a77f53a099fa02609c8f6642020a6c9494ff4681
Itay Maman <itay.maman@gmail.com>
2012-01-28 00:55:31 +0200

    initial commit of watchdog - so far a single method #ping and two tests that are somewhat cumbersome

---
+++ watchdog.rb
@@ -0,0 +1,33 @@
+describe "pinging the target" do
+  it "fetches content from target" do
+    resp = stub("response")
+    http_client = stub("http_client")
+    http_client.should_receive(:fetch).with("SOME-URL").and_return(resp)
+
+    w = WatchDog.new("SOME-URL", http_client, stub("policy").as_null_object)
+    w.ping
+  end
+
+  it "passes the received content to a policy object" do
+    policy = stub("policy")
+    policy.should_receive(:new_content).with(5000)
+
+    http_client = stub("http_client")
+    http_client.should_receive(:fetch).and_return(5000)
+
+    w = WatchDog.new("SOME-URL", http_client, policy)
+    w.ping
+  end
+end
+
+class WatchDog
+  def initialize(url, http_client, policy)
+    @url = url
+    @http_client = http_client
+    @policy = policy
+  end
+
+  def ping
+    @policy.new_content(@http_client.fetch(@url))
+  end
+end
