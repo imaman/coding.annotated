1f9c1233b3292c9520bfbd8381ff15a72d5da90f
Itay Maman <itay.maman@gmail.com>
2012-01-29 19:39:44 +0200

    RED - false -> bad in remaining two tests

--- watchdog.rb
+++ watchdog.rb
@@ -1,7 +1,7 @@
 describe "inspection of html" do
   it "fails if size is too small" do
     c = Checker.new [ :check_length ]
-    (c.check "aa").should == false
+    (c.check "aa").should == :bad
   end
   it "succeeds if size is above threshold" do
     c = Checker.new [ :check_length ]
@@ -10,7 +10,7 @@ describe "inspection of html" do
   it "allows the size threshold to be changed" do
     c = Checker.new [ :check_length ]
     c.min_length = 1001
-    c.check("a"*1000).should == false
+    c.check("a"*1000).should == :bad
   end
 
   it "fails if html does not contain NEW DESIGN STARTS HERE" do
